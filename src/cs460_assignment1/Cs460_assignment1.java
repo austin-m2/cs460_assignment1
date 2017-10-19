4b520c73b7f72127b3d490f1b171445b991006f4
austin-m2 <morris.austinm@gmail.com>
2017-10-18 23:50:02 -0700

    Added freqAttack

--- src/cs460_assignment1/Cs460_assignment1.java
+++ src/cs460_assignment1/Cs460_assignment1.java
@@ -5,6 +5,7 @@
  */
 package cs460_assignment1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -56,6 +57,12 @@ public class Cs460_assignment1 {
         
         String ciphertext = caesarEncrypt(plaintext, key);        
         System.out.println("Encrypted text:\n" + ciphertext);
+        
+        
+        freqAttack(ciphertext);
+        
+        
+        
     }
     
     public static void decrypt() {
@@ -98,6 +105,59 @@ public class Cs460_assignment1 {
             plaintext.append(newChar);
         }
         return plaintext.toString();
+    }
+    
+    public static void freqAttack(String str) {
+        double[] entropyArr  = new double[26];
+        int[] entropyKeys = new int[26];
+        
+        for (int i = 0; i < 26; i++) {
+            String ptCandidate = caesarDecrypt(str, i);
+            entropyArr[i] = getEntropy(ptCandidate);
+            entropyKeys[i] = i;
+        }
+        
+        //bubble sort both arrays
+        int n = 26;
+        double temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (entropyArr[j - 1] > entropyArr[j]) {
+                    temp = entropyArr[j - 1];
+                    entropyArr[j - 1] = entropyArr[j];
+                    entropyArr[j] = temp;
+                    
+                    temp = entropyKeys[j - 1];
+                    entropyKeys[j - 1] = entropyKeys[j];
+                    entropyKeys[j] = (int) temp;
+                }
+            }
+        }
+        
+        System.out.println("Top 10 possible plaintexts:");
+        for (int i = 0; i < 10; i++) {
+            int key = entropyKeys[i];
+            System.out.println((i + 1) + ") " + caesarDecrypt(str, key));
+        }
+        
+    }
+    
+    public static double getEntropy(String str) {
+        double[] freq = {0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228,
+                         0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025,
+                         0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987,
+                         0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150,
+                         0.01974, 0.00074};
+        
+        str = str.toUpperCase();
+        double res = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                res += -Math.log(freq[ch - 'A']);
+            }
+        }
+        return res;
     }
     
 }
