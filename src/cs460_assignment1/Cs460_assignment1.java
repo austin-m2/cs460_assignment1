e728a218e56218fdfa7871c77ddefbd4f46b2dd5
austin-m2 <morris.austinm@gmail.com>
2017-10-19 14:30:38 -0700

    added header

--- src/cs460_assignment1/Cs460_assignment1.java
+++ src/cs460_assignment1/Cs460_assignment1.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Austin Morris
+ * CS 460: Assignment 1
  */
 package cs460_assignment1;
 
